1860e8a9b45f29aea00bbc1e125507ddee05f20f
daikimorimoto <nanasinodonbei>
2024-03-20 22:31:52 +0900

    jsと削除機能追加

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,40 +4,63 @@ import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
-@RequestMapping ("user")
+@RequestMapping("user")
 public class UserController {
     private final UserService service;
-    
+
     public UserController(UserService service) {
         this.service = service;
-        
+
     }
-    
-   @GetMapping("/list")
-   public String getList(Model model) {
-       model.addAttribute("userlist",service.getUserList());
-       return "user/list";
-       
-   }
-   @GetMapping("/register")
-   public String getRegister(@ModelAttribute User user) {
-       return "user/register";
-   }
-   
-   @PostMapping("/register")
-   public String postRegister(User user) {
-       service.saveUser(user);
-       return "redirect:/user/list";
-   }
-   
+
+    @GetMapping("/list")
+    public String getList(Model model) {
+        model.addAttribute("userlist", service.getUserList());
+        return "user/list";
+
+    }
+
+    /** User登録処理 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("user", service.getUser(id));
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
+        service.saveUser(user);
+        return "redirect:/user/list";
+    }
+
+    @PostMapping(path = "list", params = "deleteRun")
+    public String deleteRun(@RequestParam(name = "idck") Set<Integer> idck, Model model) {
+        service.deleteUser(idck);
+        return "redirect:/user/list";
+
+    }
 
 }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
 import com.techacademy.entity.User;
@@ -19,10 +21,21 @@ public class UserService {
     public List<User> getUserList() {
         return userRepository.findAll();
     }
-    
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 
 }
