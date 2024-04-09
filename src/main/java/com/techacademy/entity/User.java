8749c3334a9e890bb5ac7e1859ea421348e529a7
daikimorimoto <nanasinodonbei>
2024-04-09 21:42:08 +0900

    le18課題

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,4 +1,6 @@
 package com.techacademy.entity;
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -6,7 +8,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne; 
+import jakarta.persistence.PreRemove; 
 import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -14,30 +24,42 @@ import lombok.Data;
 @Table(name = "user")
 public class User {
 
-    /** 性別用の列挙型 */
     public static enum Gender {
         男性, 女性
     }
 
-    /** 主キー。自動生成 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    /** 名前。20桁。null不許可 */
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max =20)
     private String name;
 
-    /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
-    /** 年齢 */
+    @Min(0)
+    @Max(120)
     private Integer age;
 
-    /** メールアドレス。50桁。null許可 */
     @Column(length = 50)
+    @Email
+    @Length(max=50)
     private String email;
+    
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
 
 }
